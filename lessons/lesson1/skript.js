523e9b323c6c43d6b8a8164f6f9994b1d76bba6b
Константин <kostay_mc@mail.ru>
2023-12-26 14:49:20 +0300

    lesson1

---
+++ lessons/lesson1/skript.js
@@ -0,0 +1,42 @@
+// console.log('Текст из script.js!');
+// num = 123;
+// console.log(num);
+// a = 5;
+// a = 7;
+// console.log(a);
+// b = 1 + 2 + 3;
+// console.log(b); //fduvhuf
+
+// mnsdeifjw
+
+const myAge = 18;
+const currentYear = 2023;
+let sum = currentYear + myAge;
+let dif = currentYear - myAge;
+let multiplay = currentYear * myAge;
+let div = currentYear / myAge;
+console.log(myAge, currentYear, sum, dif, multiplay, div);
+console.log(myAge + currentYear + sum + dif + multiplay + div);
+
+const a = 0.1;
+const b = 0.2;
+const d = a + b;
+console.log(Math.round(d * 10) / 10);
+const c = -6;
+console.log(-1 * c);
+console.log(-(c));
+
+const name = 'Kostya';
+const surname = 'Khait';
+console.log(name, surname);
+
+const firstWord = 'Java';
+const secondWord = 'Script';
+console.log(firstWord + secondWord);
+
+const hello = 'Hello';
+const world = 'World';
+console.log(`${hello} ${world}`)
+
+const input = Number.parseInt(prompt("Enter Number:"))
+console.log(input + 10);
